cd6c7fb5504cd454b22f4ed0e712b547f36deb98
jaystar84 <66388656+jaystar84@users.noreply.github.com>
2020-06-03 15:49:51 -0700

    resources

---
+++ cfx-server-data-master/resources/[AUTOS]/[COCHES]/Lamborghini-Urus/__resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+this_is_a_vehicle 'yes'
+
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'DLCTEXT_FILE' 'data/dlctext.meta'
+
+files {
+  'data/handling.meta',
+  'data/vehicles.meta',
+  'data/carvariations.meta',
+  'data/dlctext.meta',
+}
